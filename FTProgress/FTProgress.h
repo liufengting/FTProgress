225bacbcf41f3bf5724cfbad3853b7a5432f2efd
liufengting <wo157121900@me.com>
2018-08-24 08:53:30 +0800

    rename ftprogressview to ftprogress

---
+++ FTProgress/FTProgress.h
@@ -0,0 +1,19 @@
+//
+//  FTProgress.h
+//  FTProgress
+//
+//  Created by liufengting on 2018/8/24.
+//  Copyright © 2018年 liufengting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTProgress.
+FOUNDATION_EXPORT double FTProgressVersionNumber;
+
+//! Project version string for FTProgress.
+FOUNDATION_EXPORT const unsigned char FTProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTProgress/PublicHeader.h>
+
+
